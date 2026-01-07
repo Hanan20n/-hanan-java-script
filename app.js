9a9fd7139d2f2ad971b31e5f99ad8b0db88d8797
Hanan20n <hanan227770@gmail.com>
2026-01-07 22:45:21 +0300

    Add files via upload
    11

--- app.js
+++ app.js
@@ -1,72 +1,36 @@
-//  TRAVERSING ELEMENTS
+/* SELECT ELEMENTS */
+let title = document.getElementById("mainTitle");
+let paragraph = document.querySelector("#p1");
+let infos = document.querySelectorAll(".info");
+let list = document.getElementById("myList");
+let button = document.getElementById("btn");
+let message = document.getElementById("btnMsg");
+let output = document.getElementById("output");
 
-    let textDiv = document.querySelector(".text");
-    console.log(textDiv.lastElementChild);
+/* MODIFY */
+title.textContent = "DOM Playground (Using let)";
+paragraph.style.color = "red";
 
-    let paragraphTx = document.getElementById("tx");
-    console.log(paragraphTx.parentNode);
-    console.log(paragraphTx.previousElementSibling);
-    console.log(paragraphTx.nextElementSibling);
+/* LOOP */
+for (let info of infos) {
+  info.style.backgroundColor = "#c5b3ff";
+}
 
-    //  ACCESSING CONTENT
+/* CREATE */
+let newItem = document.createElement("li");
+newItem.textContent = "Item D (let)";
+list.appendChild(newItem);
 
-    let innerHTMLText = paragraphTx.innerHTML;
-    console.log(innerHTMLText);
+/* DELETE (after 4 seconds) */
+setTimeout(() => {
+  let lastItem = list.lastElementChild;
+  list.removeChild(lastItem);
+}, 4000);
 
-    console.log(paragraphTx.textContent);
-    console.log(paragraphTx.innerText);
+/* EVENT */
+button.addEventListener("click", function () {
+  message.textContent = "Button clicked using let ✅";
+  message.style.color = "green";
 
-    console.log(textDiv.textContent);
-    console.log(textDiv.innerText);
-
-
-    //  CHANGING CONTENT
-
-    let classDiv = document.getElementById("ca");
-    classDiv.innerHTML = "<h1>Welcome to our class CA242</h1>";
-    classDiv.textContent = "New text saved";
-
-          //  ATTRIBUTES
-         
-
-    let inputBox = document.getElementById("input");
-
-    console.log(inputBox.getAttribute("placeholder"));
-
-    inputBox.setAttribute("class", "ca242");
-    console.log(inputBox.hasAttribute("placeholder"));
-
-    inputBox.removeAttribute("placeholder");
-
-    //  CREATE NEW ELEMENT
-
-    let newDiv = document.createElement("div");
-    newDiv.innerHTML = "<p>This is the new element content</p>";
-    document.body.appendChild(newDiv);
-    newDiv.style.color = "green";
-
-    console.log(newDiv);
-
-    //  ADD LIST ITEM TO MENUE
-
-    let menu = document.querySelector(".menue");
-
-    let newListItem = document.createElement("li");
-    newListItem.textContent = "Calculus";
-    menu.appendChild(newListItem);
-
-    menu.insertAdjacentElement("afterbegin", newListItem);
-
-    //  REMOVE ELEMENT
-
-    let removeElement = document.getElementById("txt");
-    removeElement.remove();
-
-    //  REPLACE ELEMENT
-
-    let oldPara = document.getElementById("tx");
-    let newPara = document.createElement("p");
-    newPara.textContent = "This is the new replaced paragraph";
-
-    oldPara.parentNode.replaceChild(newPara, oldPara);
-
+  output.innerHTML += "<p>Button event fired</p>";
+});
